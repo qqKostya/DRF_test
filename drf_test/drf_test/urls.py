81ed6ca427bbbf25cb102ba87f733c1eb74dda3a
Kostya <leap74rus@gmail.com>
2022-02-11 23:58:27 +0300

    add deleted and REST_FRAMEWORK list

--- drf_test/drf_test/urls.py
+++ drf_test/drf_test/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from women.views import WomenAPIView, WomenAPIList
+from women.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/womenlist/', WomenAPIList.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenAPIList.as_view()),
+    path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
+    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
 ]
